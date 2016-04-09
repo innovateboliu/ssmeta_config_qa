053c544869bf07d7823ad835edaddc4dd3107f69
Bo Liu <bo.liu@openx.com>
2016-04-09 14:54:42 -0700

    first commit

---
+++ ssmeta.schema.exs
@@ -0,0 +1,27 @@
+[
+  mappings: [
+    "logger.console.backends": [
+      doc: "Provide documentation for logger.console.backends here.",
+      to: "logger.console.backends",
+      datatype: [
+        list: :atom
+      ],
+      default: [
+      ]
+    ],
+    "logger.console.log_name": [
+      doc: "Provide documentation for logger.console.log_name here.",
+      to: "logger.console.log_name",
+      datatype: :binary,
+      default: ""
+    ],
+    "kane.project_id": [
+      doc: "Provide documentation for kane.project_id here.",
+      to: "kane.project_id",
+      datatype: :binary,
+      default: ""
+    ]
+  ],
+  translations: [
+  ]
+]
